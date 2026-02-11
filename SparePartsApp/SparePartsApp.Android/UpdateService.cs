9787379ebd8abad6d71ebcfeaab956b3d6bad0a4
daniloreis <danilo.muritiba@gmail.com>
2026-02-11 14:47:58 -0300

    Add files via upload

---
+++ SparePartsApp/SparePartsApp.Android/UpdateService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using SparePartsApp.Dependencies;
+using SparePartsApp.Helpers;
+using SparePartsApp.Services;
+using Xamarin.Forms;
+
+namespace SparePartsApp.Droid
+{
+    [Service]
+    public class UpdateService : Service
+    {
+        IUpdateProvider updateProvider;
+
+        private IUpdateProvider GetupdateProvider()
+        {
+            if (updateProvider == null)
+                updateProvider = new UpdateProvider();
+
+            return updateProvider;
+        }
+
+        public override IBinder OnBind(Intent intent)
+        {
+            return null;
+        }
+
+        [return: GeneratedEnum]
+        public override StartCommandResult OnStartCommand(Intent intent, [GeneratedEnum] StartCommandFlags flags, int startId)
+        {
+            try
+            {
+                Task.Run(() =>
+                {
+                    GetupdateProvider().CheckUpdate();
+                });
+            }
+            catch (Exception)
+            {
+
+            }
+            return StartCommandResult.Sticky;
+        }
+    }
+}
